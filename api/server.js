f95ee2270c3e1bf043fc09f7dc92c26141ad0fec
Jose Moya <jose_moya@live.com.ar>
2024-04-26 19:16:22 -0300

    cors

--- api/server.js
+++ api/server.js
@@ -8,6 +8,9 @@ import connectionDB from "./data-base/db.js";
 import { router } from "./routes/index.js";
 import cookieParser from "cookie-parser";
 
+const { CORS } = process.env;
+const { SERVER_PORT } = process.env;
+
 const server = express();
 server.use(morgan("dev"));
 server.use(cookieParser());
@@ -15,13 +18,11 @@ server.use(express.json());
 
 server.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: CORS,
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
 );
-
-const { SERVER_PORT } = process.env;
 
 server.use("/api", router);
 connectionDB();
