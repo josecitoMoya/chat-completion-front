cdf5a5d28ea400c8da70b5c98696d4d41f219119
josecitoMoya <jose_moya@live.com.ar>
2024-07-25 23:24:02 +0000

    back separated

--- api/models/index.js
+++
@@ -1,4 +0,0 @@
-import UserModel from "./User.model";
-import MessageModel from "./Messages.model";
-
-export default { UserModel, MessageModel };
--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { user } from "./user.routes.js";
-import { messages } from "./messages.routes.js";
-
-export const router = Router();
-
-router.use("/chat", messages);
-router.use("/user", user);
